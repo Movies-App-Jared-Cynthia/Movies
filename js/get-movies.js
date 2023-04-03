0266fa2beab62635495a57035d630cdf8efa4003
Jared Orler <orlerdev@icloud.com>
2023-04-03 12:22:21 -0500

    added functionality to "add" button

--- js/get-movies.js
+++ js/get-movies.js
@@ -6,16 +6,23 @@ export const getMovies = async (url) => {
         const data = await res.json();
         showMovies(data.results);
     } catch (e) {
-        console.log(e.message);
+        console.log(e);
     }
 };
 
 const showMovies = (movies) => {
     main.innerHTML = "";
     movies.forEach(movie => {
-        const { title, genre_ids, poster_path, vote_average, overview } = movie;
+        const { title, id, poster_path, vote_average, overview } = movie;
         const movieElement = document.createElement("div");
         const average = Math.round(vote_average);
+        const favBtn = document.createElement("button");
+        favBtn.classList.add("fav-btn");
+        favBtn.innerText = "+";
+        favBtn.addEventListener("click", () => {
+            const { title, id, poster_path, vote_average, overview } = movie;
+            addMovie(movie);
+        });
         movieElement.classList.add("movie");
         movieElement.innerHTML = `
             <img src="${img_path + poster_path}" alt="${title}}">
@@ -28,10 +35,31 @@ const showMovies = (movies) => {
                 <span class="overview-card">${overview}</span>
             </div>
         `;
+        movieElement.appendChild(favBtn);
         main.appendChild(movieElement);
     });
+
+};
+
+const addMovie = async (movie) => {
+    const { id, title, rating } = movie;
+    try {
+        const url = `http://localhost:3000/favorites`;
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        };
+        let res = await fetch(url, options);
+        let data = await res.json();
+        return data;
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 
 const getClassByRate = (vote) => vote >= 8 ? "green" : vote >= 5 ? "orange" : "red";
 
-export { showMovies, getClassByRate };
+export { showMovies, getClassByRate, addMovie };
--- js/movies.js
+++ js/movies.js
@@ -1,18 +1,20 @@
-import { api_url, img_path, search_api, form, search } from "./movie-variables.js";
+import * as vars from "./movie-variables.js";
 import { getMovies } from "./get-movies.js";
 
 (async () => {
-    await getMovies(api_url);
+    await getMovies(vars.api_url);
 
-    form.addEventListener("submit", (e) => {
+    vars.form.addEventListener("submit", (e) => {
         e.preventDefault();
-        const searchTerm = search.value;
+        const searchTerm = vars.search.value;
         if (searchTerm && searchTerm !== "") {
-            getMovies(search_api + searchTerm);
-            search.value = "";
+            getMovies(vars.search_api + searchTerm);
+            vars.search.value = "";
         } else {
             window.location.reload();
         }
     });
-
+    
 })();
+
+
