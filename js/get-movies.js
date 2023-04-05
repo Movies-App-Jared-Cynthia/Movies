0411c80202e78529eef6485af02134c7b28ff267
Jared Orler <orlerdev@icloud.com>
2023-04-05 01:37:53 -0500

    cleaned up files using module syntax

--- js/get-movies.js
+++ js/get-movies.js
@@ -1,5 +1,7 @@
-import * as vars from "./movie-variables.js";
-import { poster, local } from "./url.js";
+import { url } from "./urls.js";
+import * as dom from "./dom.js";
+import { resetSelect, createFaveBtn, createMovieElement, createDeleteBtn, createFavoriteMovieElement, createEditBtn } from "./functions.js";
+import { editMovie} from "./edit-movies.js";
 
 export const getMovies = async (url) => {
     try {
@@ -11,7 +13,6 @@ export const getMovies = async (url) => {
         console.log(e);
     }
 };
-
 export const getFaves = async (url) => {
     try {
         const res = await fetch(url);
@@ -21,123 +22,77 @@ export const getFaves = async (url) => {
         console.log(e.message);
     }
 };
-
-export const showMovies = (movies) => {
-    vars.main.innerHTML = "";
-    movies.forEach(movie => {
-        const { title, id, poster_path, vote_average, overview } = movie;
-        const movieElement = document.createElement("div");
-        const average = Math.round(vote_average);
-        const favBtn = document.createElement("button");
-        favBtn.classList.add("fav-btn");
-        favBtn.innerHTML = `<i class="fa-sharp fa-solid fa-heart"></i>`;
-        favBtn.addEventListener("click", () => {
-            movieElement.classList.add("fave");
-            addFave(movie);
-        });
-        movieElement.classList.add("movie");
-        movieElement.innerHTML = `
-            <img src="${poster}${poster_path}" alt="${title}${'movie poster'}">
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${average}</span>
-            </div>
-            <div class="overview"> 
-                <h3>Overview</h3>
-                <span class="overview-card">${overview}</span>
-            </div>
-        `;
-        movieElement.appendChild(favBtn);
-        vars.main.appendChild(movieElement);
-    });
-};
-
-export const showFaves = (movies) => {
-    vars.main.innerHTML = "";
-    movies.forEach(movie => {
-        const { title, id, poster_path, vote_average, overview } = movie;
-        const movieElement = document.createElement("div");
-        const average = Math.round(vote_average);
-        const favBtn = document.createElement("button");
-        favBtn.classList.add("fav-btn");
-        favBtn.innerHTML = `<i class="fa-sharp fa-solid fa-heart"></i>`;
-        const deleteBtn = document.createElement("button");
-        deleteBtn.classList.add("delete-btn");
-        deleteBtn.innerHTML = `<i class="fa-solid fa-heart-crack"></i>`;
-        favBtn.addEventListener("click", () => {
-            addFave(movie);
-        });
-        favBtn.addEventListener("mouseenter", () => {
-            favBtn.classList.toggle("hide");
-        });
-        deleteBtn.addEventListener("mouseleave", () => {
-            favBtn.classList.toggle("hide");
-        });
-        deleteBtn.addEventListener("click", () => {
-            removeFave(id);
-        });
-        movieElement.classList.add("movie");
-        movieElement.innerHTML = `
-            <img src="${poster + poster_path}" alt="Movie poster for the movie ${title}">
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${average}</span>
-            </div>
-            <div class="overview">
-                <h3>Overview</h3>
-                <span class="overview-card">${overview}</span>
-            </div>
-        `;
-        movieElement.appendChild(deleteBtn);
-        movieElement.appendChild(favBtn);
-        vars.main.appendChild(movieElement);
-    });
-};
-
-export const addFave = async (movie) => {
-    const { id, overview, poster_path, title, vote_average, genre_ids } = movie;
-    let movieDetails = { id, rating: 3, title, poster_path, vote_average, overview, genre_ids };
-
+export const getUsers = async (url) => {
     try {
-        const url = local;
-        let options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(movieDetails)
-        };
-        let res = await fetch(url, options);
-        let data = await res.json();
-        console.log(options.body);
-        return data;
-    } catch (e) {
-        console.log(e.message);
-    }
-};
-
-export const removeFave = async (id) => {
-    try {
-        if (!id) {
-            throw new Error("Id required");
-        }
-        const url = `http://localhost:3000/favorites/${id}`;
-        const options = {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        };
-        const res = await fetch(url, options);
+        const res = await fetch(url);
         const data = await res.json();
-        return data;
+        showUsers(data);
     } catch (e) {
         console.log(e.message);
     }
 };
-
-export const resetSelect = () => {
-    document.querySelector("#opt1").setAttribute("selected", "selected");
+export const showMovies = (movies) => {
+    dom.main.innerHTML = "";
+    movies.forEach(movie => {
+        const { title, id, poster_path, vote_average, overview } = movie;
+        const faveBtn = createFaveBtn(movie);
+        const movieElement = createMovieElement(vote_average, poster_path, title, overview);
+        movieElement.appendChild(faveBtn);
+        dom.main.appendChild(movieElement);
+    });
+};
+export const showFaves = (movies) => {
+    dom.main.innerHTML = "";
+    movies.forEach(movie => {
+        const { title, id, poster_path, vote_average, overview } = movie;
+        const movieElement = createMovieElement(vote_average, poster_path, title, overview);
+        const faveBtn = createFaveBtn(movie, movieElement);
+        const deleteBtn = createDeleteBtn(faveBtn, id);
+        movieElement.appendChild(deleteBtn);
+        movieElement.appendChild(faveBtn);
+        dom.main.appendChild(movieElement);
+        faveBtn.addEventListener("mouseenter", () => {
+            faveBtn.classList.toggle("hide");
+        });
+    });
 };
 
-export const getClassByRate = (vote) => vote >= 8 ? "green" : vote >= 5 ? "orange" : "red";
+export const showUsers = (movies) => {
+    dom.main.innerHTML = "";
+    movies.forEach(movie => {
+        const { title, id, poster_path, vote_average, overview } = movie;
+        const movieElement = createFavoriteMovieElement(vote_average, poster_path, title, overview);
+        const average = Math.round(vote_average);
+        const faveBtn = createFaveBtn(movie, movieElement);
+        const deleteBtn = createDeleteBtn(faveBtn, id);
+        const editBtn = createEditBtn(movieElement);
+        movieElement.appendChild(deleteBtn);
+        movieElement.appendChild(faveBtn);
+        movieElement.appendChild(editBtn);
+        dom.main.appendChild(movieElement);
+        const editTitleInput = movieElement.querySelector("#edit-title-input");
+        const editRatingInput = movieElement.querySelector("#edit-rating-input");
+        const editOverviewInput = movieElement.querySelector("#edit-overview-input");
+        const editGenreInput = movieElement.querySelector("#edit-genre-input");
+        const editSubmitBtn = movieElement.querySelector("#edit-user-input-btn");
+        editSubmitBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (editTitleInput.value !== "" && editOverviewInput.value !== "") {
+                let movie = {
+                    rating: parseFloat(editRatingInput.value),
+                    title: editTitleInput.value,
+                    poster_path: `${url.userPoster}`,
+                    vote_average: parseFloat(editRatingInput.value),
+                    overview: editOverviewInput.value,
+                    genre_ids: parseFloat(editGenreInput.value)
+                };
+                console.log("event fired");
+                console.log(id);
+                console.log(movie);
+                editMovie(id, movie);
+            } else {
+                console.log("Title and Overview required");
+            }
+        });
+    });
+};
--- js/movies.js
+++ js/movies.js
@@ -1,86 +1,14 @@
-import * as vars from "./movie-variables.js";
-import { api, search, local, userLocal } from "./url.js";
-import { getFaves, getMovies } from "./get-movies.js";
-import { keys } from "./keys.js";
-import { genreInput, modal, overviewInput, ratingInput, titleInput} from "./movie-variables.js";
-import { addUser, editUser, getUsers, showUsers } from "./movies-functions.js";
+import { getMovies } from "./get-movies.js";
+import { url } from "./urls.js";
+import { addMovieBtnEventListener, overlayEventListener, overviewInputEventListener, returnHomeEventListener, searchEventListener, sortByEventListener, userSubmitBtnEventListener } from "./event-listeners.js";
 
 (async () => {
-    await getMovies(api);
-
-    vars.form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const searchTerm = vars.search.value;
-        if (searchTerm && searchTerm !== "") {
-            getMovies(search + searchTerm);
-            vars.search.value = "";
-        } else {
-            window.location.reload();
-        }
-    });
-
-    vars.sortBy.addEventListener("change", async (e) => {
-        e.preventDefault();
-        let sorted = "";
-        switch (vars.sortBy.value) {
-            case "popular":
-            case "top_rated":
-            case "now_playing":
-            case "upcoming":
-                sorted = `https://api.themoviedb.org/3/movie/${vars.sortBy.value}?api_key=${keys.tmdbV3}&language=en-US&page=1`;
-                break;
-            case "favorites":
-                await getFaves(local);
-                break;
-            case "user":
-                await getUsers(userLocal);
-                break;
-        }
-        await getMovies(sorted);
-    });
-
-    vars.overlay.addEventListener("click", (e) => {
-        vars.modal.classList.toggle("hidden");
-        vars.overlay.classList.toggle("hidden");
-        vars.body.style.overflow = "auto";
-    });
-
-    vars.modalShowBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            vars.modal.classList.toggle("hidden");
-            vars.overlay.classList.toggle("hidden");
-            if (!modal.classList.contains("hidden")) {
-                // Disable scroll
-                vars.body.style.overflow = "hidden";
-            } else {
-                // Enable scroll
-                vars.body.style.overflow = "auto";
-            }
-        }
-    )
-    ;
-
-    vars.userSubmitBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log("event fired");
-        if (titleInput.value !== "" && overviewInput.value !== "") {
-            let movie = {
-                rating: parseFloat(ratingInput.value),
-                title: titleInput.value,
-                poster_path: "./img/userPoster.png",
-                vote_average: parseFloat(ratingInput.value),
-                overview: overviewInput.value,
-                genre_ids: parseFloat(genreInput.value)
-            };
-            vars.modal.classList.toggle("hidden");
-            vars.overlay.classList.toggle("hidden");
-            addUser(movie);
-            titleInput.innerText = "";
-            overviewInput.innerText = "";
-        } else {
-            console.log("Title and Overview required");
-        }
-
-    });
-
+    await getMovies(url.api);
+    searchEventListener();
+    sortByEventListener();
+    overlayEventListener();
+    overviewInputEventListener();
+    addMovieBtnEventListener();
+    userSubmitBtnEventListener();
+    returnHomeEventListener();
 })();
